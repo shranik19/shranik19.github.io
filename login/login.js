92ce44a7c7a7e5af60586eea9e37fd98df9e04ec
Shranik Sangat <shraniksangat9@gmail.com>
2023-05-12 14:51:48 +0545

    added main content and login page

---
+++ login/login.js
@@ -0,0 +1,27 @@
+
+
+const login=document.querySelector(".btn");
+const form=document.querySelector("#form");
+
+
+form.addEventListener("submit",function(e){
+    const username=document.querySelector(".username").value;
+    const password=document.querySelector("#password").value;
+    console.log(username);
+    const pUsername="abcd";
+    const pPassword="abcd";
+    if(username==" "||username==null){
+        alert("enter the username.");
+    }
+    else if(password==""||password==null){
+        alert("enter the password");
+        e.preventDefault();
+    }
+    else if(username==pUsername && password==pPassword){
+        alert("Successfull login");
+    }
+    else{
+        alert("Invalid password.");
+        e.preventDefault();
+    }
+})
